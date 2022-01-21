3bdedd480fe1a21d5ab6719f43e6788cca582444
nicogodet <nicolas.godet@outlook.fr>
2022-01-21 23:18:56 +0100

    ci

--- .docker/start.sh
+++ .docker/start.sh
@@ -4,5 +4,5 @@ docker-compose up -d --force-recreate
 echo 'Wait 10 seconds'
 sleep 10
 echo 'Installation of the plugin'
-docker exec -t qgis sh -c "qgis_setup.sh Mesh_tools/mesh_tools"
+docker exec -t qgis sh -c "ls -lh && qgis_setup.sh Mesh_tools/mesh_tools"
 echo 'Containers are running'
