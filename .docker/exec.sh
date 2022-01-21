351a643145cfc8ceac573f3c7491d86bffa06e40
nicogodet <nicolas.godet@outlook.fr>
2022-01-21 23:37:17 +0100

    Update exec.sh

--- .docker/exec.sh
+++ .docker/exec.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-docker exec -t qgis sh -c "cd /tests_directory/tests/qgis && qgis_testrunner.sh"
+docker exec -t qgis sh -c "cd /tests_directory/ && qgis_testrunner.sh tests.qgis.test_plg_dialogs"
